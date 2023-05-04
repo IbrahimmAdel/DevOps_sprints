279dbbf8e4486abc12d504d1cfb605ca55b8ce54
ibrahim adel <ibrahim@ibrahim>
2023-05-04 02:41:59 +0100

    second task

--- python/task 2/task2.py
+++ python/task 2/task2.py
@@ -1,8 +1,10 @@
 def is_leap():
 
     year = int(input("please enter the year : "))
+    
     if year % 4 == 0:                   
-        if year % 100 == 0 and year % 400 ==0:                    
+        if year % 100 == 0
+            if year % 400 ==0:                    
                 print(" {} is a Leap year ".format(year))
         else:
             print(" {} is not a Leap year ".format(year))
