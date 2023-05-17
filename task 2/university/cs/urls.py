0ecdbcb84fe646f5c1d6850b79cb337d7ca20939
ibrahim adel <ibrahim@ibrahim>
2023-05-17 19:20:09 +0100

    microservice

--- task 2/university/cs/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('welcome/', views.welcome, name='welcome'),
-    path('students/', views.students_view, name='students'),
-    path('courses/', views.courses_view, name='courses'),
-    ]
-
